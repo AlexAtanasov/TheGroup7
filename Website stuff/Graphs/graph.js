6ae48bd233f685eabf8c25dfabdf25ab7f459063
rachelemello <rachelemello@gmail.com>
2015-12-07 12:05:15 +0100

    Simple graphs
    graph.js implements a function chart(key, div_id, json_file) that draws
    a graph of popularity and rating over time for a given movie (key) in a
    specific place (div_id) taking data from a certain source (json_file).
    graph.html offer an example of such function in use, taking data from
    the file example.json

---
+++ Website stuff/Graphs/graph.js
@@ -0,0 +1,75 @@
+function chart(key, div_id, json_file){
+  var xmlhttp = new XMLHttpRequest();
+  var url = json_file;
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      var myArray= JSON.parse(xmlhttp.responseText);
+      var myRows = rows(key, myArray);
+      drawChart(myRows);
+    }
+  };
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+
+// Takes the title of a movie (key), returns an array of rows
+var rows = function(movie, datajson) {
+  var rowsArray=[];
+  for (var i = 0; i < datajson[movie].data.length; i++) {
+    rowsArray[i] = [new Date(datajson[movie].data[i].date), 
+    parseFloat(datajson[movie].data[i].rating), 
+    parseInt(datajson[movie].data[i].popularity)]
+  };
+  return rowsArray;
+};
+
+
+google.load('visualization', '1.1', {packages: ['line', 'corechart']});
+google.setOnLoadCallback(drawChart);
+
+function drawChart(rows) {
+
+  var chartDiv = document.getElementById(div_id);
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('date', 'Date');
+  data.addColumn('number', "IMDb Rating");
+  data.addColumn('number', "Popularity");
+
+  console.log("From chart: ", rows);
+  data.addRows(rows);
+
+  var materialOptions = {
+    chart: {
+      title: 'Change in Popularity and IMDb Rating over time'
+    },
+    width: 500,
+    height: 300,
+    series: {
+      // Gives each series an axis name that matches the Y-axis below.
+      0: {axis: 'Rating'},
+      1: {axis: 'Popularity'}
+    },
+    axes: {
+      // Adds labels to each axis; they don't have to match the axis names.
+      y: {
+        Rating: {label: 'IMDb Rating (out of 10)'},
+        Popularity: {label: 'Number of Tweets'}
+      }
+    }
+  };
+
+
+  function drawMaterialChart() {
+    var materialChart = new google.charts.Line(chartDiv);
+    materialChart.draw(data, materialOptions);
+  }
+
+
+  drawMaterialChart();
+
+  };
+};
+
+
+//chart("He Named Me Malala", "chart_div", "./example.json");
